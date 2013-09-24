f118e5dae92e9f2097a1b4dbb263b978b23932a8
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-24 14:41:38 +0800

    Designed Ui and Added Icons and images needed for the project.

--- ShakeAndBreak/ShakeAndBreak/BIDViewController.h
+++ ShakeAndBreak/ShakeAndBreak/BIDViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+#import <AudioToolbox/AudioToolbox.h>
+#define kAccelerationThreshold 1.7
+#define kUpdateInterval (1.0f / 10.0f)
 
-@interface BIDViewController : UIViewController
+@interface BIDViewController : UIViewController <UIAccelerometerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (assign, nonatomic) BOOL brokenScreenShowing;
+@property (assign, nonatomic) SystemSoundID soundID;
+@property (strong, nonatomic) UIImage *fixed;
+@property (strong, nonatomic) UIImage *broken;
 
 @end
